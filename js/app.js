c0c475f6a3721a008c67537ae825c13dde289670
Matt Wilkin <mwilkin22@gmail.com>
2019-03-12 13:54:57 -0700

    add link to css/style.css in index.html and add a simple greeting message in JS to test link to js/app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 'use strict';
+var greeting = 'Howdy ';
+var name = 'Matt';
+
+var welcomeMessage = greeting + name + '!';
+
+var element = document.getElementById('greeting');
+element.textContent = welcomeMessage;
+
