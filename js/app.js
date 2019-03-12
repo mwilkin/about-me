5fb45f1bd58d4f0c7c85d98217b0015145cc583d
Matt Wilkin <mwilkin22@gmail.com>
2019-03-12 15:18:23 -0700

    Improve greeting functionality by taking the user name and displaying it.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 'use strict';
-var greeting = 'Howdy ';
-var name = 'Matt';
 
-var welcomeMessage = greeting + name + '!';
+var greeting = 'Howdy ';
+var name = prompt('What is your name');
+console.log(name);
 
 var element = document.getElementById('greeting');
+console.log(element);
+
+console.log(welcomeMessage);
+var welcomeMessage = greeting + name.toUpperCase() + '!';
 element.textContent = welcomeMessage;
+
+
+
 
