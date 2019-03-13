7190b95dfe3517c67df399dfccaf268f45c56dad
Matt Wilkin <mwilkin22@gmail.com>
2019-03-13 16:51:53 -0700

    add working question 3.

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ if(welcomeMessage.toLowerCase() === "yes" || welcomeMessage.toLowerCase() === "y
 var correctAnswers = 0;
 
 //Question 1
+
 var question1 = prompt('Do I have a dog?');
 console.log(question1);
 
@@ -35,9 +36,29 @@ if(question1.toLowerCase() === 'y' || question1.toLowerCase() === 'yes'){
   alert('Sorry sucker! You are WRONG!');
 }
 
+//Question 2
 
+var question2 = prompt('Do I like snowsports?');
+console.log(question2);
 
+if(question2.toLowerCase() === 'yes' || question2.toLowerCase() === 'y'){
+  alert('You are correct!');
+  correctAnswers += 1;
+  console.log(correctAnswers);
+} else {
+  alert('Nope. You\'re wrong. I love snowsports.');
+}
 
+//Question 3
 
+var question3 = prompt('Do I like cream with my coffee?');
+console.log(question2);
 
+if(question3.toLowerCase() === 'no' || question3.toLowerCase() === 'n'){
+  alert('You are correct!');
+  correctAnswers += 1;
+  console.log(correctAnswers);
+} else {
+  alert('Nope. You\'r wrong. I love snowsports.');
+}
 
